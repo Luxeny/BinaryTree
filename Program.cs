23c8ebce7c48ebc5cca212a3c6ef87fdee2a8080
Виталий <148930900+Luxeny@users.noreply.github.com>
2025-04-18 17:36:47 +0700

    Changed indents and translated text.

--- Program.cs
+++ Program.cs
@@ -1,61 +1,61 @@
-﻿using System;
+using System;
 using BinaryTreeCollection;
 
 class Program
 {
-    static void Main()
+  static void Main()
+  {
+    var tree = new BinaryTree<int>();
+    tree.Add(5);
+    tree.Add(3);
+    tree.Add(7);
+    tree.Add(2);
+    tree.Add(4);
+    tree.Add(6);
+    tree.Add(8);
+
+    Console.WriteLine("Прямой обход дерева (pre-order):");
+    foreach (var item in tree.PreOrder)
     {
-        var tree = new BinaryTree<int>();
-        tree.Add(5);
-        tree.Add(3);
-        tree.Add(7);
-        tree.Add(2);
-        tree.Add(4);
-        tree.Add(6);
-        tree.Add(8);
-
-        Console.WriteLine("In-order traversal (default):");
-        foreach (var item in tree)
-        {
-            Console.Write(item + " ");
-        }
-        Console.WriteLine();
-
-        Console.WriteLine("Pre-order traversal:");
-        foreach (var item in tree.PreOrder)
-        {
-            Console.Write(item + " ");
-        }
-        Console.WriteLine();
-
-        Console.WriteLine("Post-order traversal:");
-        foreach (var item in tree.PostOrder)
-        {
-            Console.Write(item + " ");
-        }
-        Console.WriteLine();
-
-        Console.WriteLine("In-order traversal with lambda:");
-        foreach (var item in tree.InOrderTraversal)
-        {
-            Console.Write(item + " ");
-        }
-        Console.WriteLine();
-
-        Console.WriteLine("Forward iteration (custom enumerator):");
-        var forward = new ForwardEnumerator<int>(tree.Root);
-        while (forward.MoveNext())
-        {
-            Console.Write(forward.Current + " ");
-        }
-        Console.WriteLine();
-
-        Console.WriteLine("Backward iteration (custom enumerator):");
-        var backward = new BackwardEnumerator<int>(tree.Root);
-        while (backward.MoveNext())
-        {
-            Console.Write(backward.Current + " ");
-        }
-        Console.WriteLine();
+      Console.Write(item + " ");
     }
+    Console.WriteLine();
+
+    Console.WriteLine("Обратный обход дерева (post-order):");
+    foreach (var item in tree.PostOrder)
+    {
+      Console.Write(item + " ");
+    }
+    Console.WriteLine();
+
+    Console.WriteLine("Симметричный обход дерева (in-order, стандартный):");
+    foreach (var item in tree)
+    {
+      Console.Write(item + " ");
+    }
+    Console.WriteLine();
+
+    Console.WriteLine("Симметричный обход через лямбда-итератор:");
+    foreach (var item in tree.InOrderTraversal)
+    {
+      Console.Write(item + " ");
+    }
+    Console.WriteLine();
+
+    Console.WriteLine("Прямой перебор (специальный итератор):");
+    var forward = new ForwardEnumerator<int>(tree.Root);
+    while (forward.MoveNext())
+    {
+      Console.Write(forward.Current + " ");
+    }
+    Console.WriteLine();
+
+    Console.WriteLine("Обратный перебор (специальный итератор):");
+    var backward = new BackwardEnumerator<int>(tree.Root);
+    while (backward.MoveNext())
+    {
+      Console.Write(backward.Current + " ");
+    }
+    Console.WriteLine();
+  }
 }
