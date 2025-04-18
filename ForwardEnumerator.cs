154be51d038fd8be857e4eeff1f08c0c20feac4e
Виталий <148930900+Luxeny@users.noreply.github.com>
2025-04-18 17:38:06 +0700

    Full implementation of in-order stack traversal.

--- ForwardEnumerator.cs
+++ ForwardEnumerator.cs
@@ -1,75 +1,70 @@
-﻿using System;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 
 namespace BinaryTreeCollection
 {
-    public class ForwardEnumerator<T> : IEnumerator<T> where T : IComparable<T>
+  public class ForwardEnumerator<T> : IEnumerator<T> where T : IComparable<T>
+  {
+    private BinaryTreeNode<T> current;
+    private readonly BinaryTreeNode<T> root;
+    private Stack<BinaryTreeNode<T>> stack;
+    private bool enumerationStarted;
+
+    public ForwardEnumerator(BinaryTreeNode<T> root)
     {
-        private BinaryTreeNode<T> current;
-        private readonly BinaryTreeNode<T> root;
-        private bool enumerationStarted;
-
-        public ForwardEnumerator(BinaryTreeNode<T> root)
-        {
-            this.root = root;
-            Reset();
-        }
-
-        public bool MoveNext()
-        {
-            if (root == null) return false;
-
-            if (!enumerationStarted)
-            {
-                current = FindLeftmostNode(root);
-                enumerationStarted = true;
-                return current != null;
-            }
-
-            if (current == null) return false;
-
-            current = GetNextNode(current);
-            return current != null;
-        }
-
-        private BinaryTreeNode<T> GetNextNode(BinaryTreeNode<T> node)
-        {
-            if (node.Right == null)
-            {
-                if (node.Parent == null || node == node.Parent.Right)
-                    return null;
-                else
-                    return node.Parent;
-            }
-            else
-            {
-                BinaryTreeNode<T> nextNode = node.Right.Left;
-                while (nextNode != null && nextNode.Left != null)
-                    nextNode = nextNode.Left;
-                return nextNode ?? node.Right;
-            }
-        }
-
-        private BinaryTreeNode<T> FindLeftmostNode(BinaryTreeNode<T> node)
-        {
-            while (node.Left != null)
-                node = node.Left;
-            return node;
-        }
-
-        public void Reset()
-        {
-            current = null;
-            enumerationStarted = false;
-        }
-
-        public T Current => current != null ? current.Value : default(T);
-        object IEnumerator.Current => Current;
-
-        public void Dispose()
-        {
-            // Nothing to dispose
-        }
+      this.root = root;
+      this.stack = new Stack<BinaryTreeNode<T>>();
+      Reset();
     }
+
+    public bool MoveNext()
+    {
+      if (root == null) return false;
+
+      if (!enumerationStarted)
+      {
+        enumerationStarted = true;
+        current = root;
+        while (current.Left != null)
+        {
+          stack.Push(current);
+          current = current.Left;
+        }
+        return true;
+      }
+
+      if (current.Right != null)
+      {
+        current = current.Right;
+        while (current.Left != null)
+        {
+          stack.Push(current);
+          current = current.Left;
+        }
+        return true;
+      }
+
+      if (stack.Count > 0)
+      {
+        current = stack.Pop();
+        return true;
+      }
+
+      current = null;
+      return false;
+    }
+
+    public void Reset()
+    {
+      current = null;
+      stack.Clear();
+      enumerationStarted = false;
+    }
+
+    public T Current => current.Value;
+    object IEnumerator.Current => Current;
+
+    public void Dispose() { }
+  }
 }
