dbeb56a8e51ec56c15fa176fa0ff5e69e5001265
Виталий <148930900+Luxeny@users.noreply.github.com>
2025-04-18 17:39:28 +0700

    Correct handling of left subtrees.

--- BackwardEnumerator.cs
+++ BackwardEnumerator.cs
@@ -1,75 +1,70 @@
-﻿using System;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 
 namespace BinaryTreeCollection
 {
-    public class BackwardEnumerator<T> : IEnumerator<T> where T : IComparable<T>
+  public class BackwardEnumerator<T> : IEnumerator<T> where T : IComparable<T>
+  {
+    private BinaryTreeNode<T> current;
+    private readonly BinaryTreeNode<T> root;
+    private Stack<BinaryTreeNode<T>> stack;
+    private bool enumerationStarted;
+
+    public BackwardEnumerator(BinaryTreeNode<T> root)
     {
-        private BinaryTreeNode<T> current;
-        private readonly BinaryTreeNode<T> root;
-        private bool enumerationStarted;
-
-        public BackwardEnumerator(BinaryTreeNode<T> root)
-        {
-            this.root = root;
-            Reset();
-        }
-
-        public bool MoveNext()
-        {
-            if (root == null) return false;
-
-            if (!enumerationStarted)
-            {
-                current = FindRightmostNode(root);
-                enumerationStarted = true;
-                return current != null;
-            }
-
-            if (current == null) return false;
-
-            current = GetPreviousNode(current);
-            return current != null;
-        }
-
-        private BinaryTreeNode<T> GetPreviousNode(BinaryTreeNode<T> node)
-        {
-            if (node.Left == null)
-            {
-                if (node.Parent == null || node == node.Parent.Left)
-                    return null;
-                else
-                    return node.Parent;
-            }
-            else
-            {
-                BinaryTreeNode<T> previousNode = node.Left.Right;
-                while (previousNode != null && previousNode.Right != null)
-                    previousNode = previousNode.Right;
-                return previousNode ?? node.Left;
-            }
-        }
-
-        private BinaryTreeNode<T> FindRightmostNode(BinaryTreeNode<T> node)
-        {
-            while (node.Right != null)
-                node = node.Right;
-            return node;
-        }
-
-        public void Reset()
-        {
-            current = null;
-            enumerationStarted = false;
-        }
-
-        public T Current => current != null ? current.Value : default(T);
-        object IEnumerator.Current => Current;
-
-        public void Dispose()
-        {
-            // Nothing to dispose
-        }
+      this.root = root;
+      this.stack = new Stack<BinaryTreeNode<T>>();
+      Reset();
     }
+
+    public bool MoveNext()
+    {
+      if (root == null) return false;
+
+      if (!enumerationStarted)
+      {
+        enumerationStarted = true;
+        current = root;
+        while (current.Right != null)
+        {
+          stack.Push(current);
+          current = current.Right;
+        }
+        return true;
+      }
+
+      if (current.Left != null)
+      {
+        current = current.Left;
+        while (current.Right != null)
+        {
+          stack.Push(current);
+          current = current.Right;
+        }
+        return true;
+      }
+
+      if (stack.Count > 0)
+      {
+        current = stack.Pop();
+        return true;
+      }
+
+      current = null;
+      return false;
+    }
+
+    public void Reset()
+    {
+      current = null;
+      stack.Clear();
+      enumerationStarted = false;
+    }
+
+    public T Current => current.Value;
+    object IEnumerator.Current => Current;
+
+    public void Dispose() { }
+  }
 }
