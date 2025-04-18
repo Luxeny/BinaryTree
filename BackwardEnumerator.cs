0de3bdae431ca994e1dbfacfdc3a78d334c98dc3
Виталий <148930900+Luxeny@users.noreply.github.com>
2025-04-18 23:49:50 +0700

    Final version with corrections to the type of variables and constructions.

--- BackwardEnumerator.cs
+++ BackwardEnumerator.cs
@@ -4,69 +4,67 @@ using System.Collections.Generic;
 
 namespace BinaryTreeCollection
 {
-  public class BackwardEnumerator<T> : IEnumerator<T> where T : IComparable<T>
-  {
-    private BinaryTreeNode<T> current;
-    private readonly BinaryTreeNode<T> root;
-    private Stack<BinaryTreeNode<T>> stack;
-    private bool enumerationStarted;
-
-    public BackwardEnumerator(BinaryTreeNode<T> root)
+    public class BackwardEnumerator<T> : IEnumerator<T> where T : IComparable<T>
     {
-      this.root = root;
-      this.stack = new Stack<BinaryTreeNode<T>>();
-      Reset();
-    }
+        private readonly BinaryTreeNode<T> _root;
+        private BinaryTreeNode<T> _current;
+        private Stack<BinaryTreeNode<T>> _stack;
+        private bool _started;
 
-    public bool MoveNext()
-    {
-        if (root == null)
+        public BackwardEnumerator(BinaryTreeNode<T> root)
         {
+            _root = root;
+            _stack = new Stack<BinaryTreeNode<T>>();
+            Reset();
+        }
+
+        public bool MoveNext()
+        {
+            if (_root == null) return false;
+
+            if (!_started)
+            {
+                _started = true;
+                _current = _root;
+                while (_current.Right != null)
+                {
+                    _stack.Push(_current);
+                    _current = _current.Right;
+                }
+                return true;
+            }
+
+            if (_current.Left != null)
+            {
+                _current = _current.Left;
+                while (_current.Right != null)
+                {
+                    _stack.Push(_current);
+                    _current = _current.Right;
+                }
+                return true;
+            }
+
+            if (_stack.Count > 0)
+            {
+                _current = _stack.Pop();
+                return true;
+            }
+
+            _current = null;
             return false;
         }
-    
-        if (!enumerationStarted)
+
+        public void Reset()
         {
-            enumerationStarted = true;
-            current = root;
-            while (current.Left != null)
-            {
-                stack.Push(current);
-                current = current.Left;
-            }
-            return true;
+            _current = null;
+            _stack.Clear();
+            _started = false;
         }
-    
-        if (current.Right != null)
-        {
-            current = current.Right;
-            while (current.Left != null)
-            {
-                stack.Push(current);
-                current = current.Left;
-            }
-            return true;
-        }
-    
-        if (stack.Count > 0)
-        {
-            current = stack.Pop();
-            return true;
-        }
-        current = null;
-        return false;
+
+        public T Current => _current != null ? _current.Value : default;
+        object IEnumerator.Current => Current;
+
+        public void Dispose() { }
     }
-
-    public void Reset()
-    {
-      current = null;
-      stack.Clear();
-      enumerationStarted = false;
-    }
-
-    public T Current => current.Value;
-    object IEnumerator.Current => Current;
-
-    public void Dispose() { }
-  }
 }
