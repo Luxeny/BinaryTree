27a905f0c145fd24a7867fd584454ecc8209e280
Виталий <148930900+Luxeny@users.noreply.github.com>
2025-04-18 23:47:34 +0700

    Final version with corrections to the type of variables and constructions.

--- BinaryTree.cs
+++ BinaryTree.cs
@@ -4,110 +4,76 @@ using System.Collections.Generic;
 
 namespace BinaryTreeCollection
 {
-  public class BinaryTree<T> : IEnumerable<T> where T : IComparable<T>
-  {
-    public BinaryTreeNode<T> Root { get; private set; }
-    public int Count { get; private set; }
-
-    public void Add(T value)
+    public class BinaryTree<T> : IEnumerable<T> where T : IComparable<T>
     {
-      if (Root == null)
-      {
-        Root = new BinaryTreeNode<T>(value);
-        ++Count;
-        return;
-      }
+        public BinaryTreeNode<T> Root { get; private set; }
+        public int Count { get; private set; }
 
-      BinaryTreeNode<T> currentNode = Root;
-      while (true)
-      {
-        int comparisonResult = value.CompareTo(currentNode.Value);
-        if (comparisonResult < 0)
+        public void Add(T value)
         {
-          if (currentNode.Left == null)
-          {
-            currentNode.Left = new BinaryTreeNode<T>(value) { Parent = currentNode };
-            ++Count;
-            return;
-          }
-          currentNode = currentNode.Left;
+            if (Root == null)
+            {
+                Root = new BinaryTreeNode<T>(value);
+                Count++;
+                return;
+            }
+
+            var currentNode = Root;
+            while (true)
+            {
+                int comparison = value.CompareTo(currentNode.Value);
+                if (comparison < 0)
+                {
+                    if (currentNode.Left == null)
+                    {
+                        currentNode.Left = new BinaryTreeNode<T>(value) { Parent = currentNode };
+                        Count++;
+                        return;
+                    }
+                    currentNode = currentNode.Left;
+                }
+                else
+                {
+                    if (currentNode.Right == null)
+                    {
+                        currentNode.Right = new BinaryTreeNode<T>(value) { Parent = currentNode };
+                        Count++;
+                        return;
+                    }
+                    currentNode = currentNode.Right;
+                }
+            }
         }
-        else
+
+        public IEnumerator<T> GetEnumerator() => new InOrderEnumerator<T>(Root);
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+        public IEnumerable<T> PreOrder => TraversePreOrder(Root);
+        public IEnumerable<T> PostOrder => TraversePostOrder(Root);
+        public IEnumerable<T> InOrderTraversal => TraverseInOrder(Root);
+
+        private IEnumerable<T> TraversePreOrder(BinaryTreeNode<T> node)
         {
-          if (currentNode.Right == null)
-          {
-            currentNode.Right = new BinaryTreeNode<T>(value) { Parent = currentNode };
-            ++Count;
-            return;
-          }
-          currentNode = currentNode.Right;
+            if (node == null) yield break;
+            yield return node.Value;
+            foreach (var left in TraversePreOrder(node.Left)) yield return left;
+            foreach (var right in TraversePreOrder(node.Right)) yield return right;
         }
-      }
-    }
 
-    public bool Contains(T value)
-    {
-      BinaryTreeNode<T> currentNode = Root;
-      while (currentNode != null)
-      {
-        int comparisonResult = value.CompareTo(currentNode.Value);
-        if (comparisonResult == 0) return true;
-        currentNode = comparisonResult < 0 ? currentNode.Left : currentNode.Right;
-      }
-      return false;
-    }
-
-    public IEnumerator<T> GetEnumerator() => new InOrderEnumerator<T>(Root);
-    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
-
-    public IEnumerable<T> PreOrder => TraversePreOrder(Root);
-    private IEnumerable<T> TraversePreOrder(BinaryTreeNode<T> node)
-    {
-      if (node == null) yield break;
-      
-      yield return node.Value;
-      foreach (T leftValue in TraversePreOrder(node.Left)) yield return leftValue;
-      foreach (T rightValue in TraversePreOrder(node.Right)) yield return rightValue;
-    }
-
-    public IEnumerable<T> PostOrder => TraversePostOrder(Root);
-    private IEnumerable<T> TraversePostOrder(BinaryTreeNode<T> node)
-    {
-      if (node == null) yield break;
-      
-      foreach (T leftValue in TraversePostOrder(node.Left)) yield return leftValue;
-      foreach (T rightValue in TraversePostOrder(node.Right)) yield return rightValue;
-      yield return node.Value;
-    }
-
-    public IEnumerable<T> InOrderTraversal => 
-      TraverseInOrder(Root, node => node.Left, node => node.Right, node => node.Value);
-
-    public static IEnumerable<T> TraverseInOrder(
-      BinaryTreeNode<T> root,
-      Func<BinaryTreeNode<T>, BinaryTreeNode<T>> leftSelector,
-      Func<BinaryTreeNode<T>, BinaryTreeNode<T>> rightSelector,
-      Func<BinaryTreeNode<T>, T> valueSelector)
-    {
-      if (root == null) yield break;
-
-      var nodeStack = new Stack<BinaryTreeNode<T>>();
-      BinaryTreeNode<T> current = root;
-
-      while (nodeStack.Count > 0 || current != null)
-      {
-        if (current != null)
+        private IEnumerable<T> TraversePostOrder(BinaryTreeNode<T> node)
         {
-          nodeStack.Push(current);
-          current = leftSelector(current);
+            if (node == null) yield break;
+            foreach (var left in TraversePostOrder(node.Left)) yield return left;
+            foreach (var right in TraversePostOrder(node.Right)) yield return right;
+            yield return node.Value;
         }
-        else
+
+        private IEnumerable<T> TraverseInOrder(BinaryTreeNode<T> node)
         {
-          current = nodeStack.Pop();
-          yield return valueSelector(current);
-          current = rightSelector(current);
+            if (node == null) yield break;
+            foreach (var left in TraverseInOrder(node.Left)) yield return left;
+            yield return node.Value;
+            foreach (var right in TraverseInOrder(node.Right)) yield return right;
         }
-      }
     }
-  }
 }
